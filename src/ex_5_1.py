2a8a6513b5fdee85bf6f9b467c509824a915114c
parvintajshaik <128246858+parvintajshaik@users.noreply.github.com>
2023-04-28 18:59:44 -0500

    Update ex_5_1.py

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -17,4 +17,12 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    desc = "This program prints the number of lines in infile."
+    
+    parser = argparse.ArgumentParser(description=desc)
+    
+    parser.add_argument("infile", type=argparse.FileType('r'))
+       
+    args = parser.parse_args()
+    
+    main(args.infile.name)
